ff59533cde3b796e8615ea02c615cf8ecb060fac
Sergio Medina <hola@sergiomedina.io>
2022-11-28 20:08:55 -0300

    feat: show modal on button click

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TITULO_MODAL = "¡Bienvenido!";
 const CONTENIDO_MODAL = "No te olvides de suscribirte al canal";
 
 const pageModal = document.getElementById("pageModal");
-pageModal.style.display = "block";
 const modalTitle = pageModal.querySelector(".modal-title");
 modalTitle.innerHTML = TITULO_MODAL;
 
@@ -14,6 +13,13 @@ const modalCtaButton = pageModal.querySelector(".modal-footer > .btn-primary");
 
 modalCloseButton.innerHTML = "Cerrar";
 modalCtaButton.innerHTML = "Aceptar";
+
+const modalCta = document.querySelector("#modalCta");
+
+modalCta.addEventListener("click", (event) => {
+    pageModal.style.display = "block";
+});
+
 
 modalCtaButton.addEventListener("click", (event) => {
     pageModal.style.display = "none";
