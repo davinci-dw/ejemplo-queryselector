cda9c237b085524c164f593534879e0fa9ef2665
Sergio Medina <hola@sergiomedina.io>
2022-11-28 19:39:52 -0300

    feat: hide modal on cta click

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TITULO_MODAL = "¡Bienvenido!";
 const CONTENIDO_MODAL = "No te olvides de suscribirte al canal";
 
 const pageModal = document.getElementById("pageModal");
-console.log(pageModal);
 pageModal.style.display = "block";
 const modalTitle = pageModal.querySelector(".modal-title");
 modalTitle.innerHTML = TITULO_MODAL;
@@ -10,7 +9,15 @@ modalTitle.innerHTML = TITULO_MODAL;
 const parraph = pageModal.querySelector(".modal-body > p");
 parraph.innerHTML = CONTENIDO_MODAL;
 
+const modalCloseButton = pageModal.querySelector(".modal-footer > .btn-secondary");
+const modalCtaButton = pageModal.querySelector(".modal-footer > .btn-primary");
+
+modalCloseButton.innerHTML = "Cerrar";
+modalCtaButton.innerHTML = "Aceptar";
+
+modalCtaButton.addEventListener("click", (event) => {
+    pageModal.style.display = "none";
+});
 
 /*setTimeout(() => {
-    pageModal.style.display = "none";
 }, 5000);*/
