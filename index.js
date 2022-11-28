f0e32c0e312936e6bcbe39fad456458a535cae29
Sergio Medina <hola@sergiomedina.io>
2022-11-28 19:29:21 -0300

    feat: show modal

---
+++ index.js
@@ -0,0 +1,7 @@
+const pageModal = document.querySelector(".modal");
+console.log(pageModal);
+pageModal.style.display = "block";
+
+/*setTimeout(() => {
+    pageModal.style.display = "none";
+}, 5000);*/
