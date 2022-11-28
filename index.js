db80acb9d51a472632eb7e8a912b156ed651339d
Sergio Medina <hola@sergiomedina.io>
2022-11-28 19:35:25 -0300

    feat: update modal content

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const pageModal = document.querySelector(".modal");
+const TITULO_MODAL = "¡Bienvenido!";
+const CONTENIDO_MODAL = "No te olvides de suscribirte al canal";
+
+const pageModal = document.getElementById("pageModal");
 console.log(pageModal);
 pageModal.style.display = "block";
+const modalTitle = pageModal.querySelector(".modal-title");
+modalTitle.innerHTML = TITULO_MODAL;
+
+const parraph = pageModal.querySelector(".modal-body > p");
+parraph.innerHTML = CONTENIDO_MODAL;
+
 
 /*setTimeout(() => {
     pageModal.style.display = "none";
